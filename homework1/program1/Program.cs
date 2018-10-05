79c09cbaa913d4ab1c88688881c5dc73119d1e29
MackHsu <786188853@qq.com>
2018-10-05 17:26:52 +0800

    第一次提交 homework1

---
+++ homework1/program1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入两个数字以求乘积：");
+            double a, b;
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            double result = a * b;
+            Console.WriteLine(a + "和" + b + "的乘积为：" + result);
+            Console.WriteLine("按下任意键结束");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ homework1/program2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            a = double.Parse(textBox1.Text);
+            b = double.Parse(textBox2.Text);
+            double result = a * b;
+            label5.Text = result.ToString();
+        }
+    }
+}
